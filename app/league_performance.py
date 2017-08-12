886c709f2edf8e38ff18d098bb1134040598589e
imosudi <imosudi@gmail.com>
2017-08-13 00:44:12 +0100

    Test Application and target application Sun Aug 13 00:44:12 WAT 2017

---
+++ app/league_performance.py
@@ -0,0 +1,7 @@
+class LeastGoalPerformer(object):
+	filename = 'football-league-results.txt'
+
+	def confirmFileIntergrity(self, filename):
+		return filename
+		pass
+		
---
+++ test/test_league_performance.py
@@ -0,0 +1,14 @@
+import unittest
+from app.league_performance import LeastGoalPerformer #import the class from target python application
+
+class TddLeastGoalPerformer(unittest.TestCase):
+
+	def setUp(self):
+		self.inputFilename = LeastGoalPerformer()
+		pass
+
+	def test__return_inputFileIntegrity(self):
+		inputFilename = LeastGoalPerformer()
+		result = inputFilename.confirmFileIntergrity('football-league-results.txt')
+		self.assertEqual('football-league-results.txt', result)
+		pass
