6de0c992d9dbeba4dfb638371a66a5c23315bd4f
imosudi <imosudi@gmail.com>
2017-08-13 17:43:14 +0100

    Unit Test 3 Resolved Sun Aug 13 17:43:14 WAT 2017

--- app/port_harcourt_weather.py
+++ app/port_harcourt_weather.py
@@ -6,7 +6,7 @@ class SmallestTemperatureSpread(object):
 
 
 	def confirmFileIntergrity(self, filename):
-		if filename.endswith('.txt') and os.path.isfile(filename):
+		if filename.endswith('.txt') and os.path.isfile(filename) and os.stat(filename).st_size > 0:
 			return filename
 		else:
 			raise ValueError
