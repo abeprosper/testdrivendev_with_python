0a8d5f79160872472af91519948591b9ca9307e2
imosudi <imosudi@gmail.com>
2017-08-13 02:09:32 +0100

    Unit Test 3 Resolved Sun Aug 13 02:09:32 WAT 2017

--- app/league_performance.py
+++ app/league_performance.py
@@ -5,13 +5,8 @@ class LeastGoalPerformer(object):
 	filename = 'football-league-results.txt'
 
 	def confirmFileIntergrity(self, filename):
-		if  filename.endswith('.txt') and os.path.isfile(filename):
-			with open(filename) as datafile:
-				first = datafile.read(1)
-				if first:
-					return filename
-				else:
-					raise ValueError
+		if  filename.endswith('.txt') and os.path.isfile(filename) and os.stat(filename).st_size > 0:
+			return filename
 		else:
 			raise ValueError
 		pass
