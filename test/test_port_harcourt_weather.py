9302e9dfac06f85172be4d6531b51d8687b10668
imosudi <imosudi@gmail.com>
2017-08-14 10:27:49 +0100

    Unit Test 7 Failed! Mon Aug 14 10:27:49 WAT 2017

--- test/test_port_harcourt_weather.py
+++ test/test_port_harcourt_weather.py
@@ -17,18 +17,18 @@ class TddSmallestTemperatureSpread(unittest.TestCase):
 		self.assertRaises(ValueError, self.inputFilename.confirmFileIntergrity, 'port-harcourt-weather')
 		pass
 
-	def test__return_error_if_resultfile_empty(self): # touch zerofile-port-harcourt-weather.txt
+	def test__return_error_if_weather_report_empty(self): # touch zerofile-port-harcourt-weather.txt
 		self.assertRaises(ValueError, self.inputFilename.confirmFileIntergrity, 'zerofile-port-harcourt-weather.txt')
 		pass
 
-	def test__return_error_if_resultfile_iteration_can_not_produce_dailyList_list(self):
+	def test__return_error_if_weather_report_iteration_can_not_produce_dailyList_list(self):
 		inputFilename = SmallestTemperatureSpread()
 		dailyList, dailyTempSpread, weatherDict = inputFilename.weatherLists('port-harcourt-weather.txt')
 		dailyList = list(dailyList) # I have a tuple here, i.e multiple return items from my method
 		self.assertIsInstance(dailyList, list)
 		pass
 
-	def test__return_error_if_resultfile_iteration_can_not_produce_dailyTempSpread_list(self):
+	def test__return_error_if_weather_report_iteration_can_not_produce_dailyTempSpread_list(self):
 		inputFilename = SmallestTemperatureSpread()
 		dailyList, dailyTempSpread, weatherDict = inputFilename.weatherLists('port-harcourt-weather.txt')
 		dailyTempSpread = list(dailyTempSpread) # I have a tuple here, i.e multiple return items
@@ -36,9 +36,16 @@ class TddSmallestTemperatureSpread(unittest.TestCase):
 		pass
 
 
-	def test__return_error_if_resultfile_iteration_can_not_produce_python_dictionary_of_day_and_temp_spread(self):
+	def test__return_error_if_weather_report_iteration_can_not_produce_python_dictionary_of_day_and_temp_spread(self):
 		inputFilename = SmallestTemperatureSpread()
 		dailyList, dailyTempSpread, weatherDict = inputFilename.weatherLists('port-harcourt-weather.txt')
 		weatherDict = dict(weatherDict) # I have a tuple here, i.e multiple return items
 		self.assertIsInstance(weatherDict, dict)
+		pass
+
+	def test__return_lowest_temp_spread(self):
+		inputFilename = SmallestTemperatureSpread()
+		dailyList, dailyTempSpread, weatherDict, smallestTemperatureSpread = inputFilename.weatherLists('port-harcourt-weather.txt')
+		smallestTemperatureSpread = int(smallestTemperatureSpread) # I have a tuple here, i.e multiple return items
+		self.assertEqual(2, smallestTemperatureSpread) # day 14 of the month(61 - 59 = 2 )
 		pass
